f80b436a73962cc2c20518ab0be46d175a66af47
tebibu21 <tebibu211@gmail.com>
2025-11-16 15:19:14 +0300

    payment functionalities

--- Amazon-Clone/src/Router.jsx
+++ Amazon-Clone/src/Router.jsx
@@ -7,14 +7,28 @@ import Orders from "./pages/Orders/Orders";
 import Cart from "./pages/Cart/Cart";
 import Results from "./pages/Results/Results"; 
 import ProductDetail from "./pages/ProductDetail/ProductDetail";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
+const stripePromise = loadStripe(
+  "pk_test_51ST14w2OdGYZ6FXHbla7xQ8iICEkJJZS0BVhvAx9FgzYOK1w77xKyBbDbg9HavwvUYKT253V8KUnatEaMGUWnMHA00yQESPwRq"
+);
 function Routing() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/auth" element={<Auth />} />
-        <Route path="/payments" element={<Payment />} />
+        <Route
+          path="/payments"
+          element={
+            <Elements
+              stripe={stripePromise}
+            >
+              <Payment />
+            </Elements>
+          }
+        />
         <Route path="/orders" element={<Orders />} />
         <Route path="/category/:categoryName" element={<Results />} />
         <Route path="/products/:productId" element={<ProductDetail />} />
--- Amazon-Clone/src/pages/Payment/Payment.jsx
+++ Amazon-Clone/src/pages/Payment/Payment.jsx
@@ -1,12 +1,85 @@
-import React from 'react'
+import React, { useContext,useState } from 'react'
 import LayOut from '../../components/LayOut/LayOut'
+import classes from "./Payment.module.css";
+import { DataContext } from '../../components/DataProvider/DataProvider';
+import ProductCard from "../../components/Product/ProductCard"
+import { useStripe, useElements, CardElement } from "@stripe/react-stripe-js";
+import CurrencyFormat from '../../components/CurrencyFormat/CurrencyFormat';
 
 function Payment() {
+  const [{basket,user}] = useContext(DataContext);
+  const totalItem = basket?.reduce((amount, item) => {
+      return item.amount + amount;
+    }, 0);
+     const total = basket.reduce(
+       (amount, item) => amount + item.price * item.amount,
+       0
+     );
+
+  const [cardError, setCardError] = useState(null);  
+   const stripe = useStripe();
+   const elements = useElements();
+
+  const handleChange = (e) => {
+    e?.error?.message? setCardError(e?.error?.message): setCardError("")
+  };
   return (
     <LayOut>
-      Payment
+      {/* header */}
+      <div className={classes.payment__header}>
+        Checkout ({totalItem}) items
+      </div>
+      {/* payment method */}
+      <section className={classes.payment}>
+        {/* address */}
+        <div className={classes.flex}>
+          <h3>Delivery Address</h3>
+          <div>
+            <div>{user?.email}</div>
+            <div>123 React Lane</div>
+            <div>Chicago, IL</div>
+          </div>
+        </div>
+        <hr />
+
+        {/* product */}
+        <div className={classes.flex}>
+          <h3>Review items and delivery</h3>
+          <div>
+            {basket?.map((item) => (
+              <ProductCard product={item} flex={true} />
+            ))}
+          </div>
+        </div>
+        <hr />
+
+        {/* card form */}
+        <div className={classes.flex}>
+          <h3>Payment Methods</h3>
+          <div className={classes.payment__card__container}>
+            <div>
+              <form action="">
+                {cardError && (
+                  <small style={{ color: "red" }}>{cardError}</small>
+                )}
+                <CardElement onChange={handleChange} />
+
+                {/* price */}
+                <div className={classes.payment__price}>
+                  <div>
+                    <span style={{display: "flex", gap: "10px"}}>
+                      <p>Total Order |</p> <CurrencyFormat amount={total} />
+                    </span>
+                  </div>
+                  <button>Pay Now</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
     </LayOut>
-  )
+  );
 }
 
 export default Payment
